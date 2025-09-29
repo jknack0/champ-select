6051575949478d2ef05576d8bc2ca247e5c796bd
jknack0 <jonathonknack@gmail.com>
2025-09-29 12:40:05 -0500

    Initial commit

---
+++ api/src/index.ts
@@ -0,0 +1,14 @@
+﻿import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+  res.json({ message: 'API is running' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
